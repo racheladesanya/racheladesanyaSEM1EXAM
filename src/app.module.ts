ebed700608535b7646ea7dc5cfc890784a5beec1
racheladesanya <72827283+racheladesanya@users.noreply.github.com>
2021-02-22 17:45:24 +0100

    message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InventoryRachelModule } from './inventory-rachel/inventory-rachel.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 
 @Module({
  
   controllers: [AppController],
   providers: [AppService],
+  imports: [InventoryRachelModule, InventoryModule],
 })
 export class AppModule {}
